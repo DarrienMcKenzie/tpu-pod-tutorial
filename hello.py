622776d26c3f3abcfa7988c495919a578225988d
DarrienMcKenzie <darrienmaurice@gmail.com>
2025-01-27 22:53:55 +0000

    Testing again.

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import jax
 def main():
     jax.distributed.initialize()
     if jax.process_index() == 0:
-        print("Hello world from tpu-pod-tutorial!")
+        print("Hello again from tpu-pod-tutorial!")
 
 
 if __name__ == "__main__":
